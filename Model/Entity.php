06d7907694319f87e1431acb9b9ead275f016559
Blanco Verdana <44454185+BTaxiB@users.noreply.github.com>
2022-04-10 15:37:04 +0200

    Delete Model directory

--- Model/Entity.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Model;
-
-final class Entity extends AbstractModel
-{
-}
--- Model/Factory/EntityFactoryException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Model\Factory;
-
-use Exception;
-
-final class EntityFactoryException extends Exception
-{
-}
