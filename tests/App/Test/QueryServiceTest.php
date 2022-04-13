eafe4c69d899891686a53c983acc3aaeff49f6d0
Blanco Verdana <44454185+BTaxiB@users.noreply.github.com>
2022-04-13 17:53:51 +0200

    Add files via upload

--- tests/App/Test/QueryServiceTest.php
+++ tests/App/Test/QueryServiceTest.php
@@ -2,17 +2,15 @@
 
 namespace App\Test;
 
-use App\Context\Context;
 use App\Context\ContextInterface;
 use App\Database\Query\Builder\QueryBuilderInterface;
 use App\Database\Query\Builder\QueryBuilderStrategy;
 use App\Database\Query\QueryService;
 use PHPUnit\Framework\TestCase;
-use ReflectionClass;
 
 class QueryServiceTest extends TestCase
 {
-    const BLOG_CONTEXT_STUB = [
+    const BLOG_CONTEXT = [
         "id",
         "title",
         "description",
@@ -25,9 +23,10 @@ class QueryServiceTest extends TestCase
         "updated_at"
     ];
 
-    public function testBuild()
+    public function testCountQueryBuild()
     {
         $expectedResult = 'SELECT COUNT(id) FROM blogs';
+
         $countQueryBuilder = $this
             ->getMockBuilder(QueryBuilderInterface::class)
             ->onlyMethods(['build'])
@@ -42,10 +41,37 @@ class QueryServiceTest extends TestCase
         $contextMock
             ->method('offsetGet')
             ->with('blogs')
-            ->willReturn(self::BLOG_CONTEXT_STUB);
+            ->willReturn(self::BLOG_CONTEXT);
 
         $sut = new QueryService($contextMock);
         $actualResult = $sut->createQuery(QueryBuilderStrategy::Count, 'blogs');
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    public function testInsertQueryBuild()
+    {
+        $params = [
+            'title' => 'test title',
+            'description' => 'test description',
+        ];
+        $expectedResult = "INSERT INTO blogs(title, description) VALUES(:title, :description)";
+        $insertQueryBuilderMock = $this
+            ->getMockBuilder(QueryBuilderInterface::class)
+            ->onlyMethods(['build'])
+            ->getMock();
+        $insertQueryBuilderMock->method('build')->willReturn($expectedResult);
+
+        $contextMock = $this
+            ->getMockBuilder(ContextInterface::class)
+            ->onlyMethods(['offsetGet'])
+            ->getMockForAbstractClass();
+        $contextMock
+            ->method('offsetGet')
+            ->with('blogs')
+            ->willReturn(self::BLOG_CONTEXT);
+
+        $sut = new QueryService($contextMock);
+        $actualResult = $sut->createQuery(QueryBuilderStrategy::Insert, 'blogs', $params);
         $this->assertEquals($expectedResult, $actualResult);
     }
 }
